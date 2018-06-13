fbff6e8742cd6b6bf3cb45622614cca38e7b9bd7
chengzichao <chengzichao@lansheng8.com>
2018-06-13 14:37:41 +0800

    搭建开发环境

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class='full-width center-content'>
+    <hello-compoent name='World'></hello-compoent>
+  </div>
+</template>
+
+<script>
+  import HelloCompoent from './components/HelloComponent.vue';
+  
+  export default {
+    components: {
+      HelloCompoent
+    }
+  }
+</script>
+
+
+
